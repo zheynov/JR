a94158f6d75c66860e5d750b8bc596eb6a18866e
Redlaw <walder@tut.by>
2016-07-06 21:03:19 +0300

    123

--- JavaRushHomeWork/src/com/javarush/test/level38/lesson10/home02/Author.java
+++ JavaRushHomeWork/src/com/javarush/test/level38/lesson10/home02/Author.java
@@ -2,6 +2,6 @@ package com.javarush.test.level38.lesson10.home02;
 
 public @interface Author {
      String value();
-    // коммит
+    // коммит прокатил
     Position position() default Position.OTHER;
 }
