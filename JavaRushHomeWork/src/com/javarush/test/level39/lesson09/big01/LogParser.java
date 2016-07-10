47f797a49fbf063bee9ff9a467e12d4808d1cc1e
Redlaw <walder@tut.by>
2016-07-11 02:29:46 +0300

    123

--- JavaRushHomeWork/src/com/javarush/test/level39/lesson09/big01/LogParser.java
+++ JavaRushHomeWork/src/com/javarush/test/level39/lesson09/big01/LogParser.java
@@ -18,15 +18,12 @@ public class LogParser implements IPQuery {
 
     @Override
     public int getNumberOfUniqueIPs(Date after, Date before) {  // возвращет количество уникальных IP адресов за выбранный перио
-
         return getUniqueIPs(after, before).size();
     }
 
     @Override
     public Set<String> getUniqueIPs(Date after, Date before) {  // множество, содержащее все неповторяющиеся IP
-
         Set<String> ips = new HashSet<>();
-
         for (String line : getLines(logDir, after, before)) {
             ips.add(line.substring(0, line.indexOf("\t")));
         }
@@ -35,34 +32,22 @@ public class LogParser implements IPQuery {
 
     @Override
     public Set<String> getIPsForUser(String user, Date after, Date before) { // IP addressess, с которых работал переданный пользователь.
-
         Set<String> ipsForUser = new HashSet<>();
-
         for (String line : getLines(logDir, after, before)) {
-
             String userIP = line.substring(0, line.indexOf("\t"));
-
             String tempUser = line.split("\t")[1];
-
             if (user != null && user.equalsIgnoreCase(tempUser))
                 ipsForUser.add(userIP);
         }
         return ipsForUser;
     }
 
-
     @Override
     public Set<String> getIPsForEvent(Event event, Date after, Date before) { // возвращает IP, с которых было произведено переданное событие
-
         Set<String> ipsForEvent = new HashSet<>();
-
         for (String line : getLines(logDir, after, before)) {
-
             String userIP = line.substring(0, line.indexOf("\t"));
-
             String eventTemp = line.split("\t")[3].trim().split(" ")[0];
-
-
             if (event != null && eventTemp.equals(event.name()))
                 ipsForEvent.add(userIP);
         }
@@ -71,15 +56,10 @@ public class LogParser implements IPQuery {
 
     @Override
     public Set<String> getIPsForStatus(Status status, Date after, Date before) {
-
         Set<String> ipsForStatus = new HashSet<>();
-
         for (String line : getLines(logDir, after, before)) {
-
             String userIP = line.substring(0, line.indexOf("\t"));
-
             String statusTemp = line.split("\t")[4].trim();
-
             if (status != null && statusTemp.equals(status.name()))
                 ipsForStatus.add(userIP);
         }
@@ -87,32 +67,19 @@ public class LogParser implements IPQuery {
     }
 
     private List<String> getLines(Path logDir, Date after, Date before) {
-
         List<File> directoryLogs = new ArrayList<>();
-
         List<String> result = new ArrayList<>();
-
         for (File file : logDir.toFile().listFiles()) {
-
             if (file.isFile() && file.getName().endsWith(".log"))
                 directoryLogs.add(file);
         }
-
         for (File directoryLog : directoryLogs) {
-
             try (BufferedReader br = new BufferedReader(new FileReader(directoryLog))) {
-
                 String line;
-
                 while ((line = br.readLine()) != null) {
-
                     String[] tempLine = line.split("\t");
-
-
                     String dateFormatted = tempLine[2];
-
                     SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.ENGLISH);
-
                     Date docDate = null;
                     try {
                         docDate = format.parse(dateFormatted);
